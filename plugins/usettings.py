400a6469fc8e7debc5a63d5a33203eff48dc66a0
songokuop <149889315+songokuop@users.noreply.github.com>
2024-06-05 10:14:26 +0530

    Update usettings.py

--- plugins/usettings.py
+++ plugins/usettings.py
@@ -42,12 +42,13 @@ async def userSettings(
             editMetadataStr = "❌"
         uSettingsMessage = f"""
 <b><u>Merge Bot settings for <a href='tg://user?id={uid}'>{fname} {lname}</a></u></b>
-    ┃
-    ┣**👦 ID: <u>{usettings.user_id}</u>**
-    ┣**{'🚫' if usettings.banned else '🫡'} Ban Status: <u>{usettings.banned}</u>**
-    ┣**{'⚡' if usettings.allowed else '❗'} Allowed: <u>{usettings.allowed}</u>**
-    ┣**{'✅' if usettings.edit_metadata else '❌'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
-    ┗**Ⓜ️ Merge mode: <u>{userMergeModeStr}</u>**
+╭──────────────────────
+┣**🎭 ID: <u>{usettings.user_id}</u>**
+┣**{'❌' if usettings.banned else '🪺'} Ban Status: <u>{usettings.banned}</u>**
+┣**{'🌦️' if usettings.allowed else '🌦️'} Allowed: <u>{usettings.allowed}</u>**
+┣**{'🏜️' if usettings.edit_metadata else '❌'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
+┣**🚏 Merge mode: <u>{userMergeModeStr}</u>**
+╰──────────────────────
 """
         markup = b.makebuttons(
             [
